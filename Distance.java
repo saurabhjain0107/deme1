21d9863290e21de1a8053c682dcd1df0ca680810
saurabh jain <luckyjain0107@gmai.com>
2022-07-04 18:36:13 +0530

    [saurabh]Distance of two point

---
+++ Distance.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.basics.deme1;
+
+public class Distance {
+    public static void main(String[] args) {
+            int x1=2 ,x2=3,y1=4,y2=6;
+            double dis;
+            dis= Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+            System.out.println(" distance between two points -->" +dis);
+        }
+    }
+
